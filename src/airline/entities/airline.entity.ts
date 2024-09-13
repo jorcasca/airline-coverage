282f066cd6f32666ebd3f94c75354f032ea682c7
jorcasca <jorgeeliecercastao@gmail.com>
2024-09-13 18:42:19 -0500

    feat(Entities): add airline and airport entities

--- src/airline/entities/airline.entity.ts
+++ src/airline/entities/airline.entity.ts
@@ -1,1 +1,24 @@
-export class Airline {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import { Airport } from '../../airport/entities/airport.entity';
+
+@Entity()
+export class Airline {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  foundationDate: Date;
+
+  @Column()
+  website: string;
+
+  @ManyToMany(() => Airport, airport => airport.airlines)
+  @JoinTable()
+  airports: Airport[];
+}
--- src/airport/entities/airport.entity.ts
+++ src/airport/entities/airport.entity.ts
@@ -1,1 +1,23 @@
-export class Airport {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
+import { Airline } from '../../airline/entities/airline.entity';
+
+@Entity()
+export class Airport {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ length: 3 })
+  code: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  city: string;
+
+  @ManyToMany(() => Airline, airline => airline.airports)
+  airlines: Airline[];
+}
