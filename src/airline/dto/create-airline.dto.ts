5ba1adbe49c5a4a22ccdd22c39cda38ce7b6b3e9
jorcasca <jorgeeliecercastao@gmail.com>
2024-09-13 18:55:44 -0500

    feat(DTOs): add airline and airport dtos

--- src/airline/dto/create-airline.dto.ts
+++ src/airline/dto/create-airline.dto.ts
@@ -1,1 +1,20 @@
-export class CreateAirlineDto {}
+import { IsString, IsNotEmpty, IsDate, IsUrl, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateAirlineDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsDate()
+    @Type(() => Date)
+    foundationDate: Date;
+
+    @IsUrl()
+    @IsOptional()
+    website?: string;
+}
--- src/airport/dto/create-airport.dto.ts
+++ src/airport/dto/create-airport.dto.ts
@@ -1,1 +1,20 @@
-export class CreateAirportDto {}
+import { IsString, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateAirportDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @Length(3, 3)
+    @IsNotEmpty()
+    code: string;
+
+    @IsString()
+    @IsNotEmpty()
+    country: string;
+
+    @IsString()
+    @IsNotEmpty()
+    city: string;
+}
